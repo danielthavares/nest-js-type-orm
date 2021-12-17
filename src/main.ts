062d348021966e79d4941d4d3bce6a21b8885873
daniel tavares <daniel.thavares@gmail.com>
2021-12-17 15:10:19 -0300

    removendo swagger

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle("My app")
-    .setDescription("My App Description")
-    .setVersion('1.0')
-    .addTag('my app tag')
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
