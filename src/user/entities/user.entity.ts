43fc6aecc61e2e968e1f9d12966bafae838eb240
daniel tavares <daniel.thavares@gmail.com>
2021-12-17 10:32:03 -0300

    configurando banco de dados

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true})
+    email: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({default: true})
+    isActive: boolean;
+
+}
